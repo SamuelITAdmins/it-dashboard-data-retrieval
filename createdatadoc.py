08c043377de070cf102bd4df762934c57c983c6c
SPozder3 <scottpozder@gmail.com>
2025-01-02 12:34:25 -0500

    Added access point data export to Excel

--- createdatadoc.py
+++ createdatadoc.py
@@ -9,11 +9,18 @@ ws = wb.active
 ws.title = "Meraki Data"
 
 # Write data to specific cells
-ws["A1"] = "Device Name"
-ws["B1"] = "Uptime Percentage"
-for i, switch_downtime in enumerate(switch_downtimes, start=2):
+row = 1
+ws[f"A{row}"] = "Device Name"
+ws[f"B{row}"] = "Uptime Percentage"
+for i, switch_downtime in enumerate(switch_downtimes, start=row+1):
     ws[f"A{i}"] = switch_downtime['device_name']
     ws[f"B{i}"] = switch_downtime['uptime_percentage']
+    row = i
+
+for i, ap_downtime in enumerate(ap_downtimes, start=row+2):
+    ws[f"A{i}"] = ap_downtime['device_name']
+    ws[f"B{i}"] = ap_downtime['uptime_percentage']
+    row = i
 
 # Save the Excel file
 file_name = "Meraki_Data.xlsx"
